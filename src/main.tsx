66d3cc2cb3978cc95ce81305d095d51ba9e63395
seba <63276982+sebaa1@users.noreply.github.com>
2026-07-02 16:00:06 -0300

    refactor: delete old css libraries

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,6 @@ import App from './App.tsx'
 
 import { AppProvider } from "./context/AppContext";
 
-import "./assets/weather-icons/styles/weather-icons.css";
-import "./assets/weather-icons/styles/weather-icons.min.css";
-import "./assets/weather-icons/styles/weather-icons-wind.css";
-import "./assets/weather-icons/styles/weather-icons-wind.min.css";
-
 import "./App.css";
 
 createRoot(document.getElementById("root")!).render(
