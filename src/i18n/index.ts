41f67efe7e3a13c7fc1e484d295b2d0038977cd9
seba <63276982+sebaa1@users.noreply.github.com>
2026-07-07 01:11:49 -0300

    fix: update package dependencies

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,11 @@
-import i18n from "i18next";
+import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 import es from "./es.json";
 import en from "./en.json";
 
-i18n
+i18next
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
@@ -24,4 +24,4 @@ i18n
         }
     });
 
-export default i18n;
+export default i18next;
